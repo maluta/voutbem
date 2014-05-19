75d5334b8284d7802f4b6a5a5a3b19da410071ba
Tiago Maluta <tiago.maluta@gmail.com>
2014-05-19 17:01:48 -0300

    Small fixes
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

--- web.js
+++ web.js
@@ -1,13 +1,29 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var bodyParser = require("body-parser");
+var path = require("path");
+var Parse = require("parse").Parse;
+
 var app = express();
 
 app.use(logfmt.requestLogger());
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+    res.render('index', {});
 });
+
+app.get('/add', function(req, res) {
+  res.send('');
+});
+
+
 
 console.log(process.env.PORT);
 var port = Number(process.env.PORT || 5000);
