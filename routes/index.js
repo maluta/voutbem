3512eb57de3b33c82c0843ee33cf0efacf097612
Tiago Maluta <tiago.maluta@gmail.com>
2014-05-25 15:17:06 -0300

    JSHINT
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,20 @@
+/*jslint node: true */
+'use scrict';
+
 var config = require('./config'); 
 
-var Parse = require("parse").Parse;
+var Parse = require('parse').Parse;
 Parse.initialize(config.keyApp,config.keyJS);
 
 Date.prototype.addHours= function(h){
-    this.setHours(this.getHours()+h);
-    return this;
-}
+  this.setHours(this.getHours()+h);
+  return this;
+}; 
 
 exports.triggerEvents = function() {
- 
- var query = new Parse.Query("Events");
- query.ascending("createdAt"); // os mais antigos primeiro
+
+ var query = new Parse.Query('Events');
+ query.ascending('createdAt'); // os mais antigos primeiro
 
  query.limit(20);
  
@@ -20,24 +23,27 @@ exports.triggerEvents = function() {
     for (var i = 0; i < events.length; i++) {
       // This does not require a network access.
       //console.log(events[i].createdAt <= events[i].createdAt.addHours(30));
+      console.log('----');
+      console.log(events[i].createdAt);
+      console.log(events[i].createdAt.addHours(30));
       if ((events[i].createdAt <= events[i].createdAt.addHours(30)) === false) { // while createAt < 30 
-           events[i].destroy({
-  				success: function(myObject) {
+       events[i].destroy({
+        success: function(myObject) {
     			// The object was deleted from the Parse Cloud.
-    			//console.log("ds");
-  			},
-  				error: function(myObject, error) {
+    			console.log('-- deleted id' + myObject.id);
+       },
+       error: function(myObject, error) {
     			// The delete failed.
     			// error is a Parse.Error with an error code and description.
-    			console.log("de");
-  				}
-  			});
-      }    
-    }
-  }
+    			console.log('-- fail --' + myObject.id );
+        }
+      });
+     }    
+   }
+ }
 });
 
-}
+}; 
 
 exports.index = function(req, res){
   res.render('index', { title: '' });
@@ -48,75 +54,75 @@ exports.add = function(req, res){
   var d = new Date();
   var today = d.getDate();
   var month = d.getMonth() + 1; // january is 0
-  res.render('add', { date: "Ex.: amanhã, dia " + today + "/"+ month });
+  res.render('add', { date: 'Ex.: amanhã, dia ' + today + '/'+ month });
 };
 
 exports.eventsId = function(req, res) {
 
-   var query = new Parse.Query("Events");
-   query.get(req.params.id, {
-            success: function(object) {
+ var query = new Parse.Query('Events');
+ query.get(req.params.id, {
+  success: function(object) {
                  //console.log("success");
                  /* mostrar quem esta indo */
                  
                  //res.send(200,"o evento existe :-) " + object.get("author"));
 
-                 res.render('event', {event_author:object.get("author"), 
-                 	                  event_title:object.get("title"), 
-                 	                  event_info:object.get("info"),
-                 	                  event_id:object.id,
-                 	                  event_guests:object.get("guests")});
-               
-            },
-            error: function(error) {
-                res.send(404, "evento (" + req.params.id + ") não encontrado ou já expirado :(");
-            }
-        });
-  
+ res.render('event', {event_author:object.get('author'), 
+   event_title:object.get('title'), 
+   event_info:object.get('info'),
+   event_id:object.id,
+   event_guests:object.get('guests')});
+
+},
+error: function(error) {
+  res.send(404, 'evento (' + req.params.id + ') não encontrado ou já expirado :(');
+}
+});
+
   // Fun fact: this has security issues
 };
 
 
 /* POST */
 exports.guests = function(req, res, next) {
- 
-   var event_id = req.originalUrl.split("/")[2];
 
-   var query = new Parse.Query("Events");
-   query.get(event_id, {
-            success: function(object) {
-                 console.log("success");
-                 /* mostrar quem esta indo */
-                 object.addUnique("guests",req.body.guestname);
-   				 
-   				 object.save(null, {
-      			 	success: function(post) {
+ var event_id = req.originalUrl.split('/')[2];
+
+ var query = new Parse.Query('Events');
+ query.get(event_id, {
+  success: function(object) {
+   console.log('success');
+   /* mostrar quem esta indo */
+   object.addUnique('guests',req.body.guestname);
+
+   object.save(null, {
+     success: function(post) {
                 	// The post was updated successfully.
                 		/*res.render('event', { event_author:object.get("author"), 
                  	    		              event_title:object.get("title"), 
                  	            		      event_info:object.get("info"),
                  	                  		  event_id:object.id,
                  	                  		  event_guests:object.get("guests")});*/
-   				         res.redirect('/event/' + event_id);
-            		},
-             		error: function(error) {
-                			res.send(404, "falha ao adicionar o usuário, tente novamente.");
-            		}
-   				 });
-            },
-            error: function(error) {
-                res.send(404, "evento (" + req.params.id + ") não encontrado ou já expirado :(");
-            }
-        });
+   res.redirect('/event/' + event_id);
+ },
+ error: function(error) {
+   res.send(404, 'falha ao adicionar o usuário, tente novamente.');
+ }
+});
+ },
+ error: function(error) {
+  res.send(404, 'evento (' + req.params.id + ') não encontrado ou já expirado :(');
+}
+});
 };
 
 exports.events = function(req, res) {
- 
+
   var event_author = req.body.who;
   var event_title = req.body.what;
   var event_info = req.body.info;
 
-  var Events = Parse.Object.extend("Events");
+  var Events = Parse.Object.extend('Events');
   var e = new Events();
   e.set('author', event_author);
   e.set('title', event_title);
@@ -124,14 +130,14 @@ exports.events = function(req, res) {
   e.set('guests', []);
 
   e.save(null, {
-            success: function(post) {
+    success: function() {
                 // The post was saved successfully.
                 //console.log("success" + e.id);
                 res.render('event_share',{ user:event_author,eventId:e.id} );
-            },
-             error: function(error) {
-                res.send(404, "falha ao criar o evento, tente novamente.");
-            }
-   });
+              },
+              error: function() {
+                res.send(404, 'falha ao criar o evento, tente novamente.');
+              }
+            });
 
 };
--- web.js
+++ web.js
@@ -1,9 +1,10 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var bodyParser = require("body-parser");
-var path = require("path");
-var routes = require('./routes')
+/*jslint node: true */
+'use scrict';
+var express = require('express');
+var logfmt = require('logfmt');
+var bodyParser = require('body-parser');
+var path = require('path');
+var routes = require('./routes');
 
 var app = express();
 
@@ -16,7 +17,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded());
 
 app.use(function(request, response, next) {
-  console.log("In comes a " + request.method + " to " + request.url);
+  console.log('In comes a ' + request.method + ' to ' + request.url);
   console.log(request.body);
   next();
 });
@@ -30,13 +31,13 @@ app.get('/event/:id', routes.eventsId);
 app.post('/events', routes.events);
 app.post('/guests/:id', routes.guests);
 
-setInterval(function() { routes.triggerEvents(); }, 30*60*60*1000)
+setInterval(function() { routes.triggerEvents(); }, 30*60*60*1000);
 
 app.all('*', function(req, res){
   res.send(404,":(");
-})
+});
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
